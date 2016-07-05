cdd81178d51253e8f1526771ed0a7f9acad38b0c
Altivi <altivi.prog@gmail.com>
2016-07-05 16:34:32 +0300

    1st time capified

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.5.0'
 
 set :application, 'chatrooomz'
 set :repo_url, 'git@54.235.152.220:web/trainee-alex-tishchenko-chat.git'
-set :deploy_to, '/home/deploy/ChatRooomz'
+set :deploy_to, '/home/deploy/chatrooomz'
 set :user, 'deploy'
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
 set :branch, :staging
