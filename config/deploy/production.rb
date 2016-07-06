9eba031eb25ba9288845f692b9f4f816b6ad3119
Altivi <altivi.prog@gmail.com>
2016-07-06 10:15:00 +0300

    database yml chandged

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # You can define all roles on a single server, or split them:
 
 
-server '52.39.91.201', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+server '52.39.91.201', user: 'deploy', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
