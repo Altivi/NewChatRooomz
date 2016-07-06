bf442d8f5dff058071ce102c20d9159d1dc9d870
Altivi <altivi.prog@gmail.com>
2016-07-06 11:27:57 +0300

    production.rb changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :format, :airbrussh
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_files, %w{config/database.yml config/application.yml}
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
