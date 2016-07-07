1c739adf84b23441db8b24ee2654b1a6c65a5d11
Altivi <altivi.prog@gmail.com>
2016-07-07 11:51:17 +0300

    email errors raising true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,8 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'new-chat-rooomz.herokuapp.com' }  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'ec2-52-39-91-201.us-west-2.compute.amazonaws.com' }  
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = false  
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {  
     address: "smtp.gmail.com",
     port: 587,
-    domain: "new-chat-rooomz.herokuapp.com",
+    domain: "ec2-52-39-91-201.us-west-2.compute.amazonaws.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_ADDRESS'],
