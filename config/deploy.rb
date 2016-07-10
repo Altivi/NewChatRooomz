5d1a2e175ec2460befa33f988c2792e0df2d5e11
Altivi <altivi.prog@gmail.com>
2016-07-11 01:19:48 +0300

    private_pub changed port and adress

--- config/deploy.rb
+++ config/deploy.rb
@@ -94,7 +94,7 @@ namespace :private_pub do
     on roles(:app) do
       within release_path do
         with rails_env: "production" do
-          execute :bundle, "exec rackup private_pub.ru -s thin -E production -d -P #{fetch(:private_pub_pid)}"
+          execute :bundle, "exec rackup private_pub.ru -s thin -E production -D -P #{fetch(:private_pub_pid)}"
         end
       end
     end
