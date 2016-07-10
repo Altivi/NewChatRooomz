affa2ad23a2dbc36827d9fb1a16ed256a240ffdb
Altivi <altivi.prog@gmail.com>
2016-07-10 23:26:26 +0300

    start private_pub task changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -94,7 +94,7 @@ namespace :private_pub do
     on roles(:app) do
       within "#{current_path}" do
         with RAILS_ENV: fetch(:production) do
-          execute :bundle, "exec rackup private_pub.ru -s thin -E production -D -P tmp/pids/private_pub.pid"
+          execute :bundle, "exec rackup private_pub.ru -s thin -E production -D -P #{fetch(:private_pub_pid)}"
         end
       end
     end
