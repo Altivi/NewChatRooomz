36bb0f3926b6cb84f2e054c9442bcfa1930552ff
Altivi <altivi.prog@gmail.com>
2016-07-12 13:55:25 +0300

    production mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,12 +64,13 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'ec2-52-39-91-201.us-west-2.compute.amazonaws.com' }  
-  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true 
   config.action_mailer.default :charset => "utf-8"  
   config.action_mailer.smtp_settings = {  
     address: "smtp.gmail.com",
     port: 587,
+    domain: "ec2-52-39-91-201.us-west-2.compute.amazonaws.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_ADDRESS'],
