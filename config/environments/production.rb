5d52307884627abc1014df6babacd3929fd5fa68
Altivi <altivi.prog@gmail.com>
2016-07-12 14:36:01 +0300

    production mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,16 +63,15 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host:'localhost', port: '3000' }  
+  config.action_mailer.default_url_options = { host: 'ec2-52-39-91-201.us-west-2.compute.amazonaws.com' }  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true 
   config.action_mailer.default :charset => "utf-8"  
   config.action_mailer.smtp_settings = {  
     address: "smtp.gmail.com",
     port: 587,
-    domain: "localhost:3000",
+    domain: "ec2-52-39-91-201.us-west-2.compute.amazonaws.com",
     authentication: "plain",
-    enable_starttls_auto: true,
     user_name: ENV['GMAIL_ADDRESS'],
     password: ENV["GMAIL_PASSWORD"]
   }
